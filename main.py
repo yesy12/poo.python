8aa8cbdafd3b6846b8e36e8926b91545deb51918
Alison <alisonvieira.av29@gmail.com>
2020-07-11 16:36:47 -0300

    Aula01.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from pessoa import Pessoa
+
+pessoa1 = Pessoa("Luiz",29)
+pessoa2 = Pessoa("Carlos",32)
+
+print(pessoa1.get_ano_nascimento())
+
+# pessoa1.comer("pão")
+# pessoa1.parar_comer()
+# pessoa1.falar("pão é muito bom")
+# pessoa1.parar_falar()
+# pessoa1.parar_falar()
+
+pessoa2.falar("água")
+pessoa2.falar("fogo")
+pessoa2.comer("pão")
